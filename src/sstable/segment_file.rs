d460535b6b7dec0059e25ead4b9c74a45c8613a1
frontseat-astronaut <frontseatastronaut@gmail.com>
2024-08-18 15:26:04 +0530

    implement compaction for sstable

--- src/sstable/segment_file.rs
+++ src/sstable/segment_file.rs
@@ -1,7 +1,8 @@
+use std::collections::VecDeque;
 use std::mem::replace;
 
 use crate::error::DbResult;
-use crate::tmp_file_names::TMP_MERGING_FILE_NAME;
+use crate::tmp_file_names::{TMP_COMPACTION_FILE_NAME, TMP_MERGING_FILE_NAME};
 use crate::{
     kv_file::{KVFile, KVLine},
     kvdb::KeyStatus,
@@ -31,18 +32,14 @@ pub struct File {
 impl SegmentFile for File {
     type Reader<'a> = Reader<'a>;
     fn set_status(&mut self, key: &str, status: &KeyStatus<String>) -> DbResult<()> {
-        self.kvfile.append_line(key, status).and_then(|offset| {
-            if should_create_new_index_entry(
-                &self.sparse_index,
-                offset,
-                self.last_indexed_offset,
-                self.sparsity,
-            ) {
-                self.sparse_index.push((key.to_owned(), offset));
-                self.last_indexed_offset = offset;
-            }
-            Ok(())
-        })
+        set_status(
+            &mut self.sparse_index,
+            &mut self.last_indexed_offset,
+            self.sparsity,
+            &mut self.kvfile,
+            key,
+            status,
+        )
     }
     fn get_status(&mut self, key: &str) -> DbResult<Option<KeyStatus<String>>> {
         get_status(&self.sparse_index, &mut self.kvfile, key)
@@ -108,24 +105,74 @@ impl SegmentFile for File {
             };
         }
 
-        let mut old_file = replace(&mut self.kvfile, new_file);
-        let file_name = old_file.file_name.clone();
-        old_file.delete()?;
-        self.kvfile.rename(&file_name)?;
-        self.sparse_index = new_index;
-        self.last_indexed_offset = last_indexed_offset;
-
+        self.replace(new_file, new_index, last_indexed_offset)?;
         Ok(())
     }
     fn rename(&mut self, new_file_name: &str) -> DbResult<()> {
         self.kvfile.rename(new_file_name)
     }
     fn compact(&mut self) -> DbResult<()> {
-        // TODO
+        let mut new_file = KVFile::new(&self.kvfile.dir_path, TMP_COMPACTION_FILE_NAME)?;
+        let mut new_index = vec![];
+        let mut last_indexed_offset = 0;
+
+        let mut flush_lines_buf = |lines_buf: &mut VecDeque<KVLine>| -> DbResult<()> {
+            while !lines_buf.is_empty() {
+                let line_to_add = lines_buf.pop_front().unwrap();
+                set_status(
+                    &mut new_index,
+                    &mut last_indexed_offset,
+                    self.sparsity,
+                    &mut new_file,
+                    &line_to_add.key,
+                    &line_to_add.status,
+                )?;
+            }
+            Ok(())
+        };
+
+        let mut index_iter = self.sparse_index.iter().peekable();
+        let mut file_iter = self.kvfile.iter()?;
+        let mut lines_buf = VecDeque::<KVLine>::new();
+        loop {
+            let maybe_next_index_entry = index_iter.peek();
+            let Some(line) = file_iter.try_next()? else {
+                flush_lines_buf(&mut lines_buf)?;
+                break;
+            };
+            if let Some((_, next_index_offset)) = maybe_next_index_entry {
+                if line.offset.eq(next_index_offset) {
+                    flush_lines_buf(&mut lines_buf)?;
+                    index_iter.next();
+                }
+            }
+            if let KeyStatus::Present(_) = line.status {
+                lines_buf.push_back(line);
+            }
+        }
+
+        self.replace(new_file, new_index, last_indexed_offset)?;
         Ok(())
     }
     fn delete(mut self) -> DbResult<()> {
         self.kvfile.delete()
+    }
+}
+
+impl File {
+    fn replace(
+        &mut self,
+        new_file: KVFile,
+        new_index: Vec<(String, u64)>,
+        last_indexed_offset: u64,
+    ) -> DbResult<()> {
+        let mut old_file = replace(&mut self.kvfile, new_file);
+        let file_name = old_file.file_name.clone();
+        old_file.delete()?;
+        self.kvfile.rename(&file_name)?;
+        self.sparse_index = new_index;
+        self.last_indexed_offset = last_indexed_offset;
+        Ok(())
     }
 }
 
@@ -206,6 +253,23 @@ fn get_status(
         }
     }
     Ok(status)
+}
+
+fn set_status(
+    sparse_index: &mut Vec<(String, u64)>,
+    last_indexed_offset: &mut u64,
+    sparsity: u64,
+    kvfile: &mut KVFile,
+    key: &str,
+    status: &KeyStatus<String>,
+) -> DbResult<()> {
+    kvfile.append_line(key, status).and_then(|offset| {
+        if should_create_new_index_entry(&sparse_index, offset, *last_indexed_offset, sparsity) {
+            sparse_index.push((key.to_owned(), offset));
+            *last_indexed_offset = offset;
+        }
+        Ok(())
+    })
 }
 
 fn should_create_new_index_entry<T>(
